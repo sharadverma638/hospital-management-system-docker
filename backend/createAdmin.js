b5db238d2f2afae32253c48546f078c66e83b50f
sharadverma638 <140721160+sharadverma638@users.noreply.github.com>
2026-08-30 09:29:15 +0000

    Use environment variable for MongoDB connection instead of hardcoded string

--- backend/createAdmin.js
+++ backend/createAdmin.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const Admin = require('./models/Admin');
 
-mongoose.connect('<your_MongoDB_connection_string>', {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
